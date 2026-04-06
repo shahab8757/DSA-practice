5ddfeb4de08e712868795ae23a3314b5be4c9544
shahabuddin alam <123970002+shahab8757@users.noreply.github.com>
2026-04-06 09:26:34 +0530

    Add files via upload

--- 2 question.cpp
+++ 2 question.cpp
@@ -1,20 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n,digit;
-    cout<<"enter the number:";
-    cin>>n;
 
-    while(n>0){
-        digit = n %10;
-        if(digit%2==0){
-            cout<<digit;
+int main() {
+    int sum = 0;
 
-       }
-        
-        n = n/10;
+    for (int i = 2; i <= 100; i += 2) {  // Loop through even numbers only
+        sum += i;  // Add i to sum
     }
+
+    cout << "Sum of all even numbers from 1 to 100 is: " << sum << endl;
+
     return 0;
 }
-
-
