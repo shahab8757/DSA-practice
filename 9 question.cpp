c2a92dd9aab3f3190538d99f2141a5672d011151
shahabuddin alam <123970002+shahab8757@users.noreply.github.com>
2026-04-19 14:08:30 +0530

    Add files via upload

--- 9 question.cpp
+++ 9 question.cpp
@@ -2,15 +2,26 @@
 using namespace std;
 
 int main() {
-    long long sum = 0;
-    int i = 1;
-    
-    while (i <= 100) {
-        sum += i * i * i;
-        i += 2;
+    int N;
+    cout << "Enter a number: ";
+    cin >> N;
+
+    int count = 0;
+    int temp = N;
+
+    if (N == 0) {  // Special case when the number itself is 0
+        count = 1;
+    } else {
+        while (temp > 0) {
+            int digit = temp % 10;  // Extract last digit
+            if (digit == 0) {
+                count++;            // Increment count if digit is 0
+            }
+            temp = temp / 10;       // Remove last digit
+        }
     }
-    
-    cout << "Sum of cubes of odd numbers from 1 to 100 is: " << sum << endl;
-    
+
+    cout << "Number of 0 digits: " << count << endl;
+
     return 0;
 }
